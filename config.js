6cdfc61093470eef6a458f643f65d46e2917e10a
whohhnb <108384401+whohhnb@users.noreply.github.com>
2023-05-27 12:44:25 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,35 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Whohh\'s Server Monitor',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm794073950-5f0c79473f6c97b27c401383',
+    'm794110748-c033b266b4b6a57f28f0a3b0',
+    'm794367351-243305619e5f6af7d3b32eba',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '梦幻星际',
+      url: 'https://www.mhxj.club/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/whohhnb'
+    },
+    {
+      text: 'GIFT',
+      url: 'https://api.whohh.cn/gift'
+    },
+  ],
+};
